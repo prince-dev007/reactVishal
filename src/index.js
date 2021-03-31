e4c6569a885aca16e4b71d8e6b9502f832528536
Prince Singh <prince.singh@telaminfotech.com>
2021-03-31 11:31:34 +0530

    check

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 

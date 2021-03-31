0e4f55e963b28c60dc97a00cc6688b485f33b107
Prince Singh <prince.singh@telaminfotech.com>
2021-03-31 11:48:10 +0530

    check

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,30 +1,8 @@
 import Header from '../Header/Header'
 import './Dashboad.css';
-import { useHistory } from "react-router-dom";
+import {Link} from 'react-router-dom';
 function Dashboard(){
-    let history = useHistory();
-    function createNewOrder()
-    {
-        history.push("/Neworder");
-    }
-    function orderHistory()
-    {
-        history.push("/OrderHistory");
-    }
-    function Outstanding()
-    {
-        history.push("/Outstanding");
-    }
-    function logoutTrans()
-    {
-        var check = window.confirm('Are you sure you want to logout');
-        if(check)
-        history.push("/");
-    }
-    function Complaint()
-    {
-        history.push("/Complaint");
-    }
+    
     return(
         
         <>
@@ -37,44 +15,44 @@ function Dashboard(){
                 quality, corroborated by National & International Standards.</p>
             <div class="row mt-3">
                 <div class="col-lg-6 col-6">
-                    <a href="#Vishal" id="newOrder" onClick={createNewOrder}><div class="content rounded shadow d-flex align-items-center justify-content-center">
+                    <Link  to="/NewOrder" id="newOrder" ><div class="content rounded shadow d-flex align-items-center justify-content-center">
                         <h1>New Order</h1>
                         <div class="spinner-border text-danger ml-2" id="orderSpinner" role="status">
                             <span class="sr-only">Loading...</span>
                           </div>
-                    </div></a>
+                    </div></Link>
                 </div>
                 <div class="col-lg-6 col-6">
-                <a href="#Vishal" id="newOrder" onClick={orderHistory}>  <div class="content rounded shadow d-flex align-items-center justify-content-center">
+                <Link to="/OrderHistory" id="newOrder" >  <div class="content rounded shadow d-flex align-items-center justify-content-center">
                         <h1>Order History</h1> 
                         <div class="spinner-border text-danger ml-1" role="status">
                             <span class="sr-only">Loading...</span>
                           </div>
-                    </div></a>
+                    </div></Link>
                 </div>
             </div>
             <div class="row mt-4">
                 <div class="col-lg-6 col-6">
-                <a href="#Vishal" id="newOrder" onClick={Outstanding}>  <div class="content rounded shadow d-flex align-items-center justify-content-center">
+                <Link to="/Outstanding"  id="newOrder" >  <div class="content rounded shadow d-flex align-items-center justify-content-center">
                         <h1>Outstanding</h1> 
                         <div class="spinner-border text-danger ml-1" role="status">
                             <span class="sr-only">Loading...</span>
                           </div>
-                    </div></a>
+                    </div></Link>
                 </div>
                 <div class="col-lg-6 col-6">
-                <a href="#Vishal" id="newOrder" onClick={Complaint}>  <div class="content rounded shadow d-flex align-items-center justify-content-center">
+                <Link to="/Complaint"  id="newOrder" >  <div class="content rounded shadow d-flex align-items-center justify-content-center">
                         <h1>Complaint</h1> 
                         <div class="spinner-border text-danger ml-1" role="status">
                             <span class="sr-only">Loading...</span>
                           </div>
-                    </div></a>
+                    </div></Link>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container text-center mt-5" onClick={logoutTrans} id="logout">
-      <button type="button" class="btn">Logout</button>
+    <div class="container text-center mt-5" id="logout">
+      <Link to="/"><button type="button" class="btn">Logout</button></Link>
           </div>
         </>
     )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,8 @@
 import Logo from '../assests/img/Vishal-Logo.jpg'
-import { useHistory } from "react-router-dom";
+
 import {Link} from 'react-router-dom';
 function Header() {
-    let history = useHistory();
-    function logout() {
-        history.push("/");
-    }
+   
     return(
         <>
             <nav class="navbar navbar-expand-lg navbar-light p-lg-3 shadow">
@@ -18,19 +15,19 @@ function Header() {
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <Link to="/Dashboard" class="nav-link" href="#Vishal">Dashboard<span class="sr-only">(current)</span></Link>
+                        <Link to="/Dashboard" class="nav-link">Dashboard<span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item active">
-                        <Link to="/OrderHistory" class="nav-link" href="#Vishal">Order History<span class="sr-only">(current)</span></Link>
+                        <Link to="/OrderHistory" class="nav-link" >Order History<span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item active">
-                        <Link to="/NewOrder" class="nav-link" href="#Vishal">New Order<span class="sr-only">(current)</span></Link>
+                        <Link to="/NewOrder" class="nav-link">New Order<span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item active">
-                        <Link to="/Outstanding" class="nav-link" href="#Vishal">Outstanding<span class="sr-only">(current)</span></Link>
+                        <Link to="/Outstanding" class="nav-link">Outstanding<span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item active">
-                        <Link to="/Complaint" class="nav-link" href="#Vishal">Complaint<span class="sr-only">(current)</span></Link>
+                        <Link to="/Complaint" class="nav-link" >Complaint<span class="sr-only">(current)</span></Link>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#Vishal" id="navbarDropdownMenuLink" role="button"
@@ -38,7 +35,7 @@ function Header() {
                             Admin Panel
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item"href="#Vishal" onClick={logout}>Logout</a>
+                            <Link to="/" class="dropdown-item">Logout</Link>
                         </div>
                     </li>
                 </ul>
